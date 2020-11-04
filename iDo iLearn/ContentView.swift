cd9f7a6c588d37bdafa33e77c44d796ae6057175
Jon Sweeney <jonsweeney26@gmil.com>
2020-11-03 17:34:44 -0800

    Initial Commit

---
+++ iDo iLearn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDo iLearn
+//
+//  Created by Jonathan Sweeney on 11/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDo iLearn/iDo_iLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDo_iLearnApp.swift
+//  iDo iLearn
+//
+//  Created by Jonathan Sweeney on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct iDo_iLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
